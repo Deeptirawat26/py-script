332312a98781de10dcd786e8d421dd68477c2376
Deeptirawat26 <149561489+Deeptirawat26@users.noreply.github.com>
2024-09-05 21:57:03 +0530

    Update script.sh

--- py-script/script.sh
+++ py-script/script.sh
@@ -3,13 +3,13 @@
 # here my root directory is defined
 root_dir="projects"
 
-# here i created and defined folders with or without oriserver
+# here i created and defined folders with or without oriserve
 mkdir -p "$root_dir/facebook"
 mkdir -p "$root_dir/google/oriserve"
 mkdir -p "$root_dir/meta/oriserve"
 mkdir -p "$root_dir/oracle"
 
-# this command finds the oriserver
+# this command finds the oriserve
 find "$root_dir" -type d -name "oriserve" | while read -r oriserve_dir; do
     # this create the txt file
     echo "Hello!!! this file is created inside: $oriserve_dir" > "$oriserve_dir/test.txt"
